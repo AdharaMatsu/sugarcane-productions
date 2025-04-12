90f8e195b3e39da481f0b83ee18143d5ada24a06
Adhara Cavazos <95373792+AdharaMatsu@users.noreply.github.com>
2025-04-11 21:17:43 -0600

    update

--- sugarcane-forecasting/scripts/pruebas.py
+++ sugarcane-forecasting/scripts/pruebas.py
@@ -1,20 +1,42 @@
 import os
+import json
 
-# Ruta de la carpeta (puede ser relativa o absoluta)
-ruta_carpeta = 'data/Mills No Weather'
+folder_path = 'data/Mills With Weather' # Mills No Weather
+json_path = 'scripts/mills_index.json'
 
-ds_store_path = os.path.join(ruta_carpeta, '.DS_Store')
+"""  Delete files
+ds_store_path = os.path.join(folder_path, '.DS_Store')
 
 if os.path.exists(ds_store_path):
     os.remove(ds_store_path)
     print(f"Se eliminó {ds_store_path}")
 else:
     print(f"No se encontró {ds_store_path}")
+"""
+info = os.listdir(folder_path)
 
-# Obtener lista de archivos y directorios
-contenido = os.listdir(ruta_carpeta)
+current_files = [name_file for name_file in info if os.path.isfile(os.path.join(folder_path, name_file))]
 
-# Filtrar solo archivos (opcional)
-archivos = [nombre for nombre in contenido if os.path.isfile(os.path.join(ruta_carpeta, nombre))]
+with open(json_path, 'r', encoding='utf-8') as f:
+    data = json.load(f)
 
-print(archivos)
+for ID, info in data.items():
+    complete_mill_name = f"{info['name']}.csv"
+    id_mill_name = complete_mill_name.strip().lower()
+    new_mill_name = f"{ID}.csv"
+
+    # Buscar el archivo real haciendo comparación insensible a mayúsculas
+    real_file = None
+    for files in current_files:
+        if files.strip().lower() == id_mill_name:
+            real_file = files
+            break
+
+    if real_file:
+        current_path = os.path.join(folder_path, real_file)
+        new_path = os.path.join(folder_path, new_mill_name)
+
+        os.rename(current_path, new_path)
+        print(f"✅ Renameded : {real_file} → {new_mill_name}")
+    else:
+        print(f"❌ No encontrado en carpeta: {complete_mill_name}")
